edf5036926439fa8d01d94773ef2a1ec1497bd84
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-09-25 21:53:37 +1200

    change

--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Link, TextField } from '@mui/material'
+import { Button, Link, TextField } from '@mui/material'
 import { LoginState } from '../types'
 import { DatePicker } from '@mui/x-date-pickers'
 import { useState } from 'react'
