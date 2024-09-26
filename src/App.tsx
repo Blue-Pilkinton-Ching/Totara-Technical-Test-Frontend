b2ebc71f035ad2bafe8481dab3bc0bfe03a996bd
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-09-26 15:17:21 +1200

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,26 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { Button, CircularProgress } from '@mui/material'
+import Dashboard from './components/dashboard'
 
 function App() {
-  const { user, isAuthenticated, isLoading, loginWithRedirect, logout, error } =
-    useAuth0()
+  const { isAuthenticated, isLoading, loginWithRedirect, error } = useAuth0()
 
   return (
     <>
       <div className="App">
         <main className="h-svh w-full flex items-center justify-center font-lato ">
-          <div className="rounded-2xl shadow-lg p-7 sm:p-10 bg-gray-100 w-[min(400px,100vw)] gap-4 flex-col flex flex-center">
-            {isLoading ? (
-              <CircularProgress className="mx-auto" />
-            ) : isAuthenticated ? (
-              <>
-                <h1 className="text-3xl">You're Logged In!</h1>
-                <br />
-                <div className="flex flex-row items-center gap-5">
-                  <img
-                    className="w-12 rounded"
-                    src={user?.picture}
-                    alt={user?.name}
-                  />
-                  <h2 className="flex-1 text-center pb-2 text-lg text-ellipsis max-w-full overflow-clip min-w-0">
-                    {user?.name}
-                  </h2>
-                  <h3>{user?.birthdate}</h3>
-                </div>
-                <br />
-                <Button
-                  variant="contained"
-                  color="primary"
-                  fullWidth
-                  onClick={() =>
-                    logout({
-                      logoutParams: { returnTo: window.location.origin },
-                    })
-                  }
-                >
-                  Log Out
-                </Button>
-              </>
-            ) : error ? (
-              <>
-                <h1 className="text-3xl">An Error occurred!</h1>
-                <h2 className="text-lg">{error.message}</h2>
-              </>
-            ) : (
-              <>
+          {isLoading ? (
+            <CircularProgress className="mx-auto" />
+          ) : isAuthenticated ? (
+            <Dashboard />
+          ) : error ? (
+            <>
+              <h1 className="text-3xl">An Error occurred!</h1>
+              <h2 className="text-lg">{error.message}</h2>
+            </>
+          ) : (
+            <>
+              <div className="rounded-2xl shadow-lg p-7 sm:p-10 bg-gray-100 w-[min(400px,100vw)] gap-4 flex-col flex flex-center">
                 <h1 className="text-3xl">You're Logged Out!</h1>
                 <br />
                 <Button
@@ -58,9 +31,9 @@ function App() {
                 >
                   Log In
                 </Button>
-              </>
-            )}
-          </div>
+              </div>
+            </>
+          )}
         </main>
       </div>
     </>
